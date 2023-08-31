85f5db771285d21de8572cef74161d0847021c7e
Jonas Bosse <jonas.bosse@posteo.de>
2023-08-31 18:27:04 +0200

    reading csv values

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <ostream>
 #include "vawt/aerofoil.hpp"
+#include "external/csv-parser/single_include/csv.hpp"
 
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
+
+
+    csv::CSVFormat format;
+    format.no_header().delimiter(',');
+    csv::CSVReader reader("examples/NACA0018/NACA0018Re0040.data", format);
+    for (csv::CSVRow& row: reader) {
+        for (csv::CSVField& field: row ) {
+            std::cout << field.get<>();
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "bye" << std::endl;
+    
     return 0;
 }
