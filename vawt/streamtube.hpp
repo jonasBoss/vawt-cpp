c9b2b5b07d5efc2638328499be3dd0965b655e7a
Jonas Bosse <jonas.bosse@posteo.de>
2023-09-13 17:55:17 +0200

    add StreamTubeSolution

--- vawt/streamtube.hpp
+++ vawt/streamtube.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 #include "vawt.hpp"
+#include <boost/math/constants/constants.hpp>
+#include <cmath>
+#include <memory>
 #include <tuple>
 #include <utility>
 namespace vawt {
+class StreamTubeSolution;
 
 class StreamTube {
+    friend StreamTubeSolution;
+
   private:
     double a_0;
     double theta;
@@ -125,6 +131,88 @@ class StreamTube {
      * @return double
      */
     double solve_a(VAWTCase case_, double epsilon);
+};
+
+class StreamTubeSolution {
+  private:
+    VAWTCase case_;
+    StreamTube tube;
+    double _a;
+    StreamTubeSolution(VAWTCase case_, StreamTube tube, double a)
+        : case_(case_), tube(tube), _a(a) {}
+
+  public:
+    /**
+     * @brief the induction factor of the solution
+     *
+     * @return double
+     */
+    double a() { return this->_a; }
+
+    /**
+     * @brief the induction factor of the upstream streamtube
+     *
+     * @return double
+     */
+    double a_0() { return this->tube.a_0; }
+
+    /**
+     * @brief the pitch angel in radians
+     *
+     * @return double
+     */
+    double beta() { return this->tube.beta; }
+
+    /**
+     * @brief the stramtube location in radinas
+     *
+     * @return double
+     */
+    double theta() { return this->tube.theta; }
+
+    /**
+     * @brief the relative windspeed at the foil
+     *
+     * @return double
+     */
+    double w() { return this->tube.w_vec(this->a(), this->case_).magnitude(); }
+
+    /**
+     * @brief the angle of attac at the foil
+     *
+     * @return double
+     */
+    double alpha() {
+        auto [w_x_foil, w_y_foil] = this->tube.w_vec(this->a(), this->case_)
+                                        .to_foil(this->theta(), this->beta());
+        return atan2(w_y_foil, w_x_foil) +
+               boost::math::double_constants::pi / 2.0;
+    }
+
+    /**
+     * @brief the local reynolds number at the foil
+     *
+     * @return double
+     */
+    double re() { return this->w() * this->case_.re; }
+
+    /**
+     * @brief the difference between the wind thrust and the foil force of the
+     * solution
+     *
+     * @return double
+     */
+    double thrust_error() {
+        return this->tube.thrust_error(this->a(), this->case_);
+    }
+
+    /**
+     * @brief tangential foil coefficient
+     *
+     * coefficient of lift and drag evaluated in tangential direction
+     * @return double
+     */
+    double c_tan() { return this->tube.c_tan(this->a(), this->case_); }
 };
 
 } // namespace vawt
