67f2d10a7eccd2aaa5516bd049bae576877ecf15
Jonas Bosse <jonas.bosse@posteo.de>
2023-09-21 17:33:32 +0200

    main for perf measurements

--- main.cpp
+++ main.cpp
@@ -1,67 +1,39 @@
-#include <aerofoil.hpp>
-#include <algorithm>
 #include <boost/math/constants/constants.hpp>
+#include <vawt.hpp>
+#include <chrono>
 #include <iostream>
-#include <ostream>
-#include <vector>
 
+using namespace vawt;
 const double TO_RAD = boost::math::double_constants::pi / 180;
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, world!" << std::endl;
-
+static std::shared_ptr<Aerofoil> load_naca0018() {
     vawt::AerofoilBuilder* builder = new vawt::AerofoilBuilder;
+    return builder->load_data("examples/NACA0018/NACA0018Re0080.data", 80'000.0)
+        .load_data("examples/NACA0018/NACA0018Re0040.data", 40'000.0)
+        .load_data("examples/NACA0018/NACA0018Re0160.data", 160'000.0)
+        .set_aspect_ratio(12.8)
+        .update_aspect_ratio(true)
+        .symmetric(true)
+        .build();
+}
 
-    auto aerofoil =
-        builder->load_data("examples/NACA0018/NACA0018Re0080.data", 80000.0)
-            .load_data("examples/NACA0018/NACA0018Re0040.data", 40000.0)
-            .load_data("examples/NACA0018/NACA0018Re0160.data", 160000.0)
-            .set_aspect_ratio(12.8)
-            .update_aspect_ratio(true)
-            .symmetric(true)
-            .build();
+static VAWTSolver setup_solver(std::shared_ptr<Aerofoil> foil) {
+    auto testcase = VAWTSolver(foil);
+    testcase.re(31'300.0).solidity(0.3525).n_streamtubes(72).tsr(3.25);
+    return testcase;
+}
 
-    std::vector<double> alpha(45);
-    std::vector<double> re(20);
+int main(int argc, char** argv) {
+    auto foil = load_naca0018();
+    auto testcase = setup_solver(foil);
 
-    double step = 90.0 / 44.0 * TO_RAD;
-    std::generate(alpha.begin(), alpha.end(), [i = 0.0, step]() mutable {
-        double current = i;
-        i += step;
-        return current;
-    });
-
-    step = (180000.0 - 30000.0) / 19;
-    std::generate(re.begin(), re.end(), [i = 30000.0, step]() mutable {
-        double current = i;
-        i += step;
-        return current;
-    });
-
-    std::cout << "cl = [";
-    for (auto alpha : alpha) {
-        for (auto re : re) {
-            auto cl = aerofoil->cl_cd(alpha, re).cl();
-            std::cout << cl << ", ";
-        }
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i<10'000; i++){
+        testcase.solve(0.0);
     }
-    std::cout << "]" << std::endl;
-
-    std::cout << "alpha = [";
-    for (auto alpha : alpha) {
-        for (auto re : re) {
-            std::cout << alpha << ", ";
-        }
-    }
-    std::cout << "]" << std::endl;
-
-    std::cout << "re = [";
-    for (auto alpha : alpha) {
-        for (auto re : re) {
-            std::cout << re << ", ";
-        }
-    }
-    std::cout << "]" << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Duration for 10'000 solutions: " << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }
